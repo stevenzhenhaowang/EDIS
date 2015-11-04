50e14d5599a77e13a4ad4632a9cb39805d5cae10
stevenzhenhaowang <zwang.steven@mail.com>
2015-11-04 20:45:23 +1100

    assetPrice

---
+++ EDISAngular/Domain.Portfolio/AggregateRoots/Asset/AssetPrice.cs
@@ -0,0 +1,21 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Portfolio.AggregateRoots.Asset
+{
+
+    public class AssetPrice
+    {
+        public string Id { get; set; }
+        public double? Price { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public AssetTypes AssetType { get; set; }
+        public string CorrespondingAssetKey { get; set; }
+
+
+    }
+}
